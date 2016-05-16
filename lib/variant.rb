e86c23e05126387248647d237bec3bb77e3db960
Mohnish G J <mail4mohnish@gmail.com>
2016-05-16 06:27:03 +0530

    Add full title to a variant
    Full title of a variant is the product name concatenated with the
    variant title.

--- lib/variant.rb
+++ lib/variant.rb
@@ -1,8 +1,9 @@
 class Variant
-  attr_reader :id, :title, :product_id, :price, :grams
+  attr_reader :id, :title, :product_id, :price, :grams, :full_title
 
-  def initialize(id, title, product_id, price, grams)
+  def initialize(id, title, product_id, price, grams, product_title)
     @id, @title, @product_id = id, title, product_id
     @price, @grams =  price.to_f, grams
+    @full_title = product_title.concat(" ").concat(title)
   end
 end
--- spec/variant_spec.rb
+++ spec/variant_spec.rb
@@ -1,6 +1,7 @@
 describe Variant do
   describe "#initialize" do
-    let(:variant) { Variant.new(8041889923, "Magenta", 2759196675, 14.30, 3030) }
+    let(:variant) { Variant.new(8041889923, "Magenta", 2759196675,
+                                14.30, 3030, "Heavy Duty Concrete Keyboard") }
 
     context "Given a Variant with id, title, product_id, price and weight in grams" do
       it "should set the variant related attributes appropriately" do
@@ -9,6 +10,7 @@ describe Variant do
         expect(variant.product_id).to eq(2759196675)
         expect(variant.price).to eq(14.30)
         expect(variant.grams).to eq(3030)
+        expect(variant.full_title).to eq("Heavy Duty Concrete Keyboard Magenta")
       end
     end
   end
