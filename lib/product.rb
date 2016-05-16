2f4d1848e06061bd6d709d580f962ea7e0a9c97a
Mohnish G J <mail4mohnish@gmail.com>
2016-05-16 07:58:20 +0530

    Pass the product title to help get a variant's full title

--- lib/product.rb
+++ lib/product.rb
@@ -3,15 +3,15 @@ class Product
 
   def initialize(id, title, product_type, product_variants)
     @id, @title, @product_type = id, title, product_type
-    @variants = get_variants(product_variants)
+    @variants = get_variants(product_variants, title)
   end
 
   private
 
-    def get_variants(product_variants)
+    def get_variants(product_variants, title)
       product_variants.each_with_object([]) do |variant, variants|
         variants << Variant.new(variant["id"], variant["title"], variant["product_id"],
-                                variant["price"], variant["grams"])
+                                variant["price"], variant["grams"], title)
       end
     end
 end
