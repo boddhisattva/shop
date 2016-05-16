492714f7b116353dada5b87548c1b7c4031d81f6
Mohnish G J <mail4mohnish@gmail.com>
2016-05-16 06:31:50 +0530

    Remove reading from unused attributes
    Variant attributes like id, title and product_id aren’t currently
    read/used from other parts of the shop program, hence removing them for
    now.

--- lib/variant.rb
+++ lib/variant.rb
@@ -1,5 +1,5 @@
 class Variant
-  attr_reader :id, :title, :product_id, :price, :grams, :full_title
+  attr_reader :price, :grams, :full_title
 
   def initialize(id, title, product_id, price, grams, product_title)
     @id, @title, @product_id = id, title, product_id
--- spec/variant_spec.rb
+++ spec/variant_spec.rb
@@ -5,9 +5,6 @@ describe Variant do
 
     context "Given a Variant with id, title, product_id, price and weight in grams" do
       it "should set the variant related attributes appropriately" do
-        expect(variant.id).to eq(8041889923)
-        expect(variant.title).to eq("Magenta")
-        expect(variant.product_id).to eq(2759196675)
         expect(variant.price).to eq(14.30)
         expect(variant.grams).to eq(3030)
         expect(variant.full_title).to eq("Heavy Duty Concrete Keyboard Magenta")
