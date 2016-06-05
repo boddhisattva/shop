aad68aa59b033118dd5c1ec397b99bd4d599e57c
Mohnish G J <mail4mohnish@gmail.com>
2016-06-05 16:02:39 +0530

    Remove extra empty line

--- spec/lib/customer_spec.rb
+++ spec/lib/customer_spec.rb
@@ -1,6 +1,5 @@
 describe Customer do
   describe "#initialize" do
-
     context "Given a Customer with name and id" do
       it "should set the Customer related attributes appropriately" do
         customer = Customer.new("Alice", 1)
