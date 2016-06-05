e7f97e03a93d77f4683c036aa73c939772830ed5
Mohnish G J <mail4mohnish@gmail.com>
2016-06-05 16:04:51 +0530

    Do not use parallel assignment as its less readable than separate assignment

--- lib/variant.rb
+++ lib/variant.rb
@@ -2,8 +2,11 @@ class Variant
   attr_reader :price, :grams, :full_title
 
   def initialize(product_title, variant = {})
-    @id, @title, @product_id = variant["id"], variant["title"], variant["product_id"]
-    @price, @grams = variant["price"].to_f, variant["grams"]
+    @id = variant["id"]
+    @title = variant["title"]
+    @product_id = variant["product_id"]
+    @price = variant["price"].to_f
+    @grams = variant["grams"]
     @full_title = product_title + " " + @title
   end
 end
