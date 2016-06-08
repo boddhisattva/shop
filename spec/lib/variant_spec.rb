e2230e5a6b3dd31d172742edc3085eba51f12cde
Mohnish G J <mail4mohnish@gmail.com>
2016-06-08 09:13:56 +0530

    Fix long test description and incorrect ones
    Given a number of ‘product variants’ means that the variants belong to
    only one product which needn’t be the case always, hence fixing them.

--- spec/lib/variant_spec.rb
+++ spec/lib/variant_spec.rb
@@ -14,7 +14,7 @@ describe Variant do
   end
 
   describe "#initialize" do
-    context "Given a Variant with id, title, product_id, price and weight in grams" do
+    context "Given a variant with certain attributes" do
       it "should set the variant related attributes appropriately" do
         expect(variant.price).to eq(14.30)
         expect(variant.grams).to eq(3030)
@@ -24,7 +24,7 @@ describe Variant do
   end
 
   describe '.sort_by_ascending_price' do
-    context "Given a number of product variants" do
+    context "Given a number of variants" do
       it "sorts the variants based in ascending price order" do
         expect(Variant.sort_by_ascending_price(variants)).to eq([variant2, variant])
       end
@@ -32,7 +32,7 @@ describe Variant do
   end
 
   describe '.total_cost' do
-    context "Given a number of product variants" do
+    context "Given a number of variants" do
       it "calculates the total cost of all those variants" do
         expect(Variant.total_cost(variants)).to eq(25.6)
       end
