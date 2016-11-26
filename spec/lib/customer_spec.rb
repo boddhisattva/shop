555e037ea3d24009f01600d22d7b09443529a012
Mohnish G J <mail4mohnish@gmail.com>
2016-11-26 15:25:55 +0530

    [experimenting] Test a method without using a test framework
    In order to test say, using your own test framework, you simply need to
    use a one line one method.
    
    Credits - Learnt this from Corey Haines

--- spec/lib/customer_spec.rb
+++ spec/lib/customer_spec.rb
@@ -1,11 +1,23 @@
-describe Customer do
-  describe "#initialize" do
-    context "Given a Customer with name and id" do
-      it "should set the Customer related attributes appropriately" do
-        customer = Customer.new("Alice", 1)
+require_relative '../../lib/customer'
 
-        expect(customer.id).to eq(1)
-      end
-    end
-  end
+def assert(conditions) raise "Fail" unless conditions ; end
+
+# describe Customer do
+#   describe "#initialize" do
+#     describe "Given a Customer with name and id" do
+#       it "should set the Customer related attributes appropriately" do
+#         customer = Customer.new("Alice", 1)
+
+#         # expect(customer.id).to eq(1)
+#       end
+#     end
+#   end
+# end
+
+def test_initialize
+  customer = Customer.new("Alice", 1)
+
+  assert(customer.id == 1)
 end
+
+test_initialize
